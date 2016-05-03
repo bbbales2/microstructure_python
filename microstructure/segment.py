70acf5e55838f706cb7ac898c9ee22f67338ec9b
Ben Bales <bbbales2@gmail.com>
2016-05-03 11:18:08 -0700

    Partial commit to save some code

--- microstructure/segment.py
+++ microstructure/segment.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy
 import util
+import tempfile
 
 class CahnHilliard():
     """This is a class for simulating a binary spinodal decomposition (useful for segmenting superalloy microstructure images) using a Cahn-Hilliard PDE on a 2D grid.
@@ -37,7 +38,7 @@ class CahnHilliard():
     In the Cahn-Hilliard equation, c is conserved. For segmentation applications, this is important. If you're trying to segment a y-y' microstructure that is 90% y and 10% y', this'll be a problem and you'll want to play around with the mean_shift parameter in the run function (if it's 50% y and 50% y', you're probably okay with a default mean_shift)!
 
     It runs on Nvidia GPUs using Google Tensorflow"""
-    def __init__(self, shape):
+    def __init__(self, shape, dt = 0.01, y = 0.25):
         """The shape of the Cahn-Hilliard PDE is specified at construction. This just allocates memory and builds the Tensorflow compute graph.
 
         Arguments:
@@ -49,8 +50,8 @@ class CahnHilliard():
 
         self.shape = shape
 
-        self.y = tf.placeholder(tf.complex64)
-        self.dt = tf.placeholder(tf.complex64)
+        self.y = y
+        self.dt = dt
 
         Wx = 2.0 * numpy.pi * numpy.fft.fftfreq(shape[1], 1.0)
         Wy = 2.0 * numpy.pi * numpy.fft.fftfreq(shape[0], 1.0)
@@ -67,17 +68,19 @@ class CahnHilliard():
         self.ix = tf.real(tf.ifft2d(self.x))
         self.x3 = tf.fft2d(tf.complex(self.ix * self.ix * self.ix, self.zeros))
 
-        self.xold = tf.Variable(numpy.zeros(shape).astype('complex64'))
-        self.saveX = self.xold.assign(self.x)
+        self.xOld = tf.Variable(numpy.zeros(shape).astype('complex64'))
+        self.saveX = self.xOld.assign(self.x)
 
-        self.update1 = self.x.assign(self.x / (1.0 + self.dt * (self.wx2 + self.wy2) * (-1.0 + self.y * (self.wx2 + self.wy2))))
+        self.implicit = self.x / (1.0 + self.dt * (self.wx2 + self.wy2) * (-1.0 + self.y * (self.wx2 + self.wy2)))
+
+        self.update1 = self.x.assign(self.implicit)
         self.update2 = self.x.assign_sub(self.dt * (self.wx2 + self.wy2) * self.x3)
 
-        self.computeChange = tf.reduce_mean(tf.complex_abs(self.xold - self.x))
+        self.computeChange = tf.reduce_mean(tf.complex_abs(self.xOld - self.x))
 
         self.initialize = self.x.assign(tf.fft2d(tf.complex(self.xinp, self.zeros)))
 
-    def run(self, im, stopping_threshold = 1e-4, max_steps = 50, dt = 0.01, y = 0.25, mean_shift = 0.0):
+    def run(self, im, stopping_threshold = 1e-4, max_steps = 50, mean_shift = 0.0):
         """Run the Cahn-Hilliard PDE
 
         Arguments:
@@ -102,7 +105,7 @@ class CahnHilliard():
 
         with tf.Session() as sess:
             losses = []
-            sess.run(tf.initialize_all_variables())
+            sess.run(tf.initialize_variables([self.wx2, self.wy2, self.x, self.xOld]))
             sess.run(self.initialize, feed_dict = { self.xinp : im })
 
             import time
@@ -110,8 +113,8 @@ class CahnHilliard():
             for i in range(max_steps):
                 tmp = time.time()
                 sess.run(self.saveX)
-                sess.run(self.update1, feed_dict = { self.dt : dt, self.y : y })
-                sess.run(self.update2, feed_dict = { self.dt : dt, self.y : y })
+                sess.run(self.update1)
+                sess.run(self.update2)
                 print time.time() - tmp
                 losses.append(sess.run(self.computeChange))
 
@@ -121,31 +124,57 @@ class CahnHilliard():
                 #plt.colorbar()
                 #plt.show()
 
-            plt.plot(losses)
-            plt.show()
+            #plt.plot(losses)
+            #plt.show()
 
-        return sess.run(self.ix)
+            output = sess.run(self.ix)
 
-    def fitV(self, im, vShape, dt = 0.01, y = 0.25, mean_shift = 0.0, max_steps = 100, stopping_threshold = 1e-4):
-        print vShape, len(vShape)
+        return output
+
+class CahnHilliardWithV(CahnHilliard):
+    def __init__(self, shape, vShape = [13, 13], dt = 0.01, y = 0.25):
+        CahnHilliard.__init__(self, shape = shape, dt = dt, y = y)
+
+        if len(shape) != 2:
+            raise Exception("shape must be length 2")
+
         if len(vShape) != 2:
             raise Exception("vShape must be length 2")
 
+        self.shape = shape
         self.vShape = vShape
 
+        # Set up the network for figuring out V
         self.V = tf.Variable(tf.truncated_normal([vShape[0], vShape[1], 1, 1], 0.1))
+        self.nx1 = tf.Variable(numpy.zeros(self.shape).astype('complex64'))
+        self.rnx1 = tf.Variable(numpy.zeros([1, self.shape[0], self.shape[1], 1]).astype('float32'))
 
-        self.ix2 = tf.reshape(self.ix, [1, self.shape[0], self.shape[1], 1])
-        self.xV = tf.nn.conv2d(self.ix2, self.V, [1, 1, 1, 1], padding = 'SAME')
+        self.computeImplicit = self.nx1.assign(self.implicit)
+        self.computeIFFT = self.rnx1.assign(tf.reshape(tf.real(tf.ifft2d(self.nx1)), [1, self.shape[0], self.shape[1], 1]))
+
+        self.xV = tf.nn.conv2d(self.rnx1, self.V, [1, 1, 1, 1], padding = 'SAME')
 
         self.fftV = tf.fft2d(tf.complex(tf.reshape(self.xV, self.shape), self.zeros))
 
-        self.nx1 = self.x / (1.0 + self.dt * (self.wx2 + self.wy2) * (-1.0 + self.y * (self.wx2 + self.wy2)))
         self.nx2 = self.nx1 - self.dt * (self.wx2 + self.wy2) * (self.x3 + self.fftV)
 
         self.error = tf.reduce_mean(tf.complex_abs(self.nx2 - self.x))
 
-        train_step = tf.train.AdamOptimizer(1e-2).minimize(self.error, var_list = [self.V])
+        self.train_step = tf.train.AdamOptimizer(1e-2).minimize(self.error, var_list = [self.V])
+
+        # Modify the parent update functions to use the V
+
+        self.xV2 = tf.nn.conv2d(tf.reshape(tf.real(tf.ifft2d(self.x)), [1, self.shape[0], self.shape[1], 1]), self.V, [1, 1, 1, 1], padding = 'SAME')
+        self.fftV2 = tf.fft2d(tf.complex(tf.reshape(self.xV2, self.shape), self.zeros))
+
+        self.update2 = self.x.assign_sub(self.dt * (self.wx2 + self.wy2) * (self.x3 + self.fftV2))
+
+        self.saver = tf.train.Saver({ "V" : self.V })
+
+        self.is_fit = False
+
+    def fit(self, im, mean_shift = 0.0, max_steps = 1000, stopping_threshold = 1e-4):
+        self.is_fit = True
 
         import matplotlib.pyplot as plt
         im = util.rescale(im, -1.0 + mean_shift, 1.0 + mean_shift)
@@ -155,32 +184,95 @@ class CahnHilliard():
             sess.run(tf.initialize_all_variables())
             sess.run(self.initialize, feed_dict = { self.xinp : im })
 
+            sess.run(self.saveX)
+            sess.run(self.update1)
+            sess.run(self.update2)
+
+            inv = (self.dt * (self.wx2 + self.wy2)).eval()
+            inv[0, 0] = 1.0
+
+            M = ((self.x - self.xOld) / inv).eval()
+            M[0, 0] = 0.0
+
+            sess.run(self.toFit.assign(numpy.real(numpy.fft.ifft2(M))))
+
             import time
+
+            sess.run(self.computeImplicit)
+            sess.run(self.computeIFFT)
 
             for i in range(max_steps):
                 tmp = time.time()
-                train, error = sess.run([train_step, self.error], feed_dict = { self.dt : dt, self.y : y })
+                train, error = sess.run([self.train_step, self.error])
                 print time.time() - tmp
  
                 losses.append(error)
 
                 print "Error", error
 
-                if i % 100 == 0 and i != 0:#len(losses) > 1 and numpy.abs((losses[-2] - losses[-1]) / losses[-2]) < stopping_threshold:
-                    plt.plot(losses)
-                    plt.show()
-                    plt.imshow(self.V.eval()[:, :, 0, 0], cmap = plt.cm.gray, interpolation = 'NONE')
-                    plt.show()
+                if len(losses) > 1 and numpy.abs((losses[-2] - losses[-1]) / losses[-2]) < stopping_threshold:
+                    #print stopping_threshold
+                    #plt.plot(losses)
+                    #plt.show()
+                    break
+
+            if i == max_steps - 1:
+                print "Warning: Fit exited at max_steps limit, may have not converged"
+
+                #if i % 250 == 0 and i != 0:
+                    #plt.plot(losses)
+                    #plt.show()
+                    #plt.imshow(self.V.eval()[:, :, 0, 0], cmap = plt.cm.gray, interpolation = 'NONE')
+                    #plt.colorbar()
+                    #plt.show()
                     #import pdb
                     #pdb.set_trace()
-                    #break
+
+            output = sess.run(self.V)[:, :, 0, 0]
+
+            self.tmpVFile = tempfile.NamedTemporaryFile()
+
+            self.saver.save(sess, self.tmpVFile.name)
+        return output 
+
+    def run(self, im, stopping_threshold = 1e-4, max_steps = 50, mean_shift = 0.0):
+        import matplotlib.pyplot as plt
+        im = util.rescale(im, -1.0 + mean_shift, 1.0 + mean_shift)
+
+        if not self.is_fit:
+            print "Model is not fit yet. Please run CahnHilliardWithV.fit first"
+            return
+
+        with tf.Session() as sess:
+            losses = []
+            sess.run(tf.initialize_all_variables())
+            sess.run(self.initialize, feed_dict = { self.xinp : im })
+            self.saver.restore(sess, self.tmpVFile.name)
+
+            #import pdb
+            #pdb.set_trace()
+
+            import time
+
+            for i in range(max_steps):
+                tmp = time.time()
+                sess.run(self.saveX)
+                sess.run(self.update1)
+                sess.run(self.update2)
+                print time.time() - tmp
+                losses.append(sess.run(self.computeChange))
+
+                if len(losses) > 1 and numpy.abs(losses[-2] - losses[-1]) < stopping_threshold:
+                    #import pdb
+                    #pdb.set_trace()
+                    break
                 #plt.imshow(sess.run(self.ix))
                 #plt.colorbar()
                 #plt.show()
 
-            plt.plot(losses)
-            plt.show()
+            #plt.plot(losses)
+            #plt.show()
 
-        return sess.run(self.ix)
+            output = sess.run(self.ix)
 
-        pass
+        return output
--- test_seg.py
+++ test_seg.py
@@ -7,23 +7,49 @@ import matplotlib.pyplot as plt
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-im = skimage.color.rgb2gray(skimage.io.imread('molybdenum1.png')[:1024, :1024])
+im = skimage.color.rgb2gray(skimage.io.imread('/home/bbales2/rafting/nrafting2a/images_9/signal0.png'))[:256, :256]
+im = skimage.color.rgb2gray(skimage.io.imread('/home/bbales2/lukestuff/flat/ReneN5_1238_1079_2pct_10000x_Transverse_DendriteCore.TIF'))[:800, :800]
+#im = skimage.color.rgb2gray(skimage.io.imread('molybdenum3.png'))[:1024, :1024]
 
-im = skimage.transform.resize(im, [512, 512])
+im = skimage.transform.resize(im, [256, 256])
 
 print im.shape
 
 # Perform a Cahn Hilliard segmentation
-ch = microstructure.segment.CahnHilliard(im.shape)
+ch = microstructure.segment.CahnHilliard(im.shape, y = 0.25, dt = 0.01)
 
-#seg = ch.run(im, max_steps = 100, y = 0.05)
-
-ch.fitV(im, [25, 25], max_steps = 10000, dt = 0.1)
+seg1 = ch.run(im, max_steps = 100)
 
 # Visualize the segmentation
 #plt.imshow(im, cmap = plt.cm.gray, interpolation = 'NONE')
-plt.imshow(seg, cmap = plt.cm.gray, interpolation = 'NONE')
+#plt.imshow(seg1, cmap = plt.cm.jet, interpolation = 'NONE')
+#plt.colorbar()
+#plt.show()
+
+# Perform a Cahn Hilliard segmentation
+ch = microstructure.segment.CahnHilliardWithV(im.shape, [13, 13], y = 0.25, dt = 0.01)
+
+V = ch.fit(im, max_steps = 100000)
+
+seg2 = ch.run(im, max_steps = 10)
+
+plt.imshow(V, interpolation = 'NONE')
 plt.colorbar()
+plt.show()
+
+# Visualize the segmentation
+plt.subplot(1, 3, 0)
+plt.imshow(im, cmap = plt.cm.jet, interpolation = 'NONE')
+plt.title('Original')
+#plt.colorbar()
+plt.subplot(1, 3, 1)
+plt.imshow(seg1, cmap = plt.cm.jet, interpolation = 'NONE')
+plt.title('Seg 1')
+#plt.colorbar()
+plt.subplot(1, 3, 2)
+plt.imshow(seg2, cmap = plt.cm.jet, interpolation = 'NONE')
+plt.title('Seg 2')
+#plt.colorbar()
 plt.show()
 
 exit(0)
