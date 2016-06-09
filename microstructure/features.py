1809f8f0b31c177937b8891b4d371f8bbf0be19d
Ben Bales <bbbales2@gmail.com>
2016-06-09 16:30:49 -0700

    Adding another HOG feature descriptor

--- microstructure/features.py
+++ microstructure/features.py
@@ -1,7 +1,35 @@
 import pyximport
-pyximport.install()
+pyximport.install(reload_support=True)
 
-from hog import hog
+import hog
+import skimage.filters
+
+def hog2(im, bins = 20, stride = 1, sigma = 0.0):
+    Nios = im.shape[0] / stride
+    Njos = im.shape[1] / stride
+
+    imp = numpy.pad(im, 1, mode = 'reflect').astype('float')
+
+    if sigma > 0.0:
+        imp = skimage.filters.gaussian(imp, sigma)
+    
+    output = numpy.zeros((Nios, Njos, bins), dtype = 'float')
+
+    for io in range(Nios):
+        i = io * stride + 1
+        for jo in range(Njos):
+            j = jo * stride + 1
+
+            dy = (imp[i + 1, j] - imp[i - 1, j]) / 2.0
+            dx = (imp[i, j + 1] - imp[i, j - 1]) / 2.0
+            
+            angle = (numpy.arctan2(dy, dx) + numpy.pi) / (2.0 * numpy.pi)
+            
+            b = int(min(bins - 1, numpy.floor(angle * bins)))
+            
+            output[io, jo, b] += numpy.sqrt(dx * dx + dy * dy)
+
+    return output
 
 class HOG():
     def __init__(self, binsize = 8, padding = False):
